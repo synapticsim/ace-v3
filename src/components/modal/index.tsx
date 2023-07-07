6a1944dd3a128942b72e5516f1b275f13d54589b
Alexander <sashok935@gmail.com>
2023-07-07 22:57:37 +0300

    Drop event consts, fix modal outside click closing

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,9 +1,6 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { FiX } from 'react-icons/fi';
 import { AnimatePresence, motion, Variants } from 'framer-motion';
-
-const KEY_NAME_ESC = 'Escape';
-const KEY_EVENT_TYPE = 'keyup';
 
 const modalBgAnimation: Variants = {
     hidden: {
@@ -35,20 +32,32 @@ export interface ModalProps {
 }
 
 export const Modal: React.FC<ModalProps> = ({ title, show, onExit, children }) => {
+    const ref = useRef<HTMLDivElement>(null);
+
     // Close modal on Esc press
     const handleEscKey = useCallback((event: KeyboardEvent) => {
-        if (event.key === KEY_NAME_ESC) {
+        if (event.key === 'Escape') {
             onExit();
         }
     }, [onExit]);
 
+    // Close modal on outside click
+    const handleClick = useCallback((event: MouseEvent) => {
+        // @ts-ignore EventTarget is not mapping on Node but works just fine
+        if (ref?.current?.contains && !ref.current.contains(event.target)) {
+            onExit();
+        }
+    }, [onExit, ref]);
+
     useEffect(() => {
-        document.addEventListener(KEY_EVENT_TYPE, handleEscKey);
+        document.addEventListener('keyup', handleEscKey);
+        document.addEventListener('mouseup', handleClick);
 
         return () => {
-            document.removeEventListener(KEY_EVENT_TYPE, handleEscKey);
+            document.removeEventListener('keyup', handleEscKey);
+            document.removeEventListener('mouseup', handleClick);
         };
-    }, [handleEscKey]);
+    }, [handleClick, handleEscKey]);
 
     return (
         <AnimatePresence>
@@ -60,7 +69,6 @@ export const Modal: React.FC<ModalProps> = ({ title, show, onExit, children }) =
                     initial="hidden"
                     animate="visible"
                     exit="hidden"
-                    onClick={onExit}
                 >
                     <motion.div
                         className="relative max-w-1/2 mx-auto bg-silver-800 rounded-2xl shadow-2xl"
@@ -74,6 +82,7 @@ export const Modal: React.FC<ModalProps> = ({ title, show, onExit, children }) =
                         initial="hidden"
                         animate="visible"
                         exit="hidden"
+                        ref={ref}
                     >
                         <div className="px-8 py-5 bg-silver-700 rounded-t-2xl flex justify-between items-center">
                             <h3 className="font-medium">{title}</h3>
