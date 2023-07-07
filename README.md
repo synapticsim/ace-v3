ea14cb1c373d6e7ea453567eba5816177bae28ea
Alexander <sashok935@gmail.com>
2023-07-07 11:42:19 +0300

    chore: Drop yarn, update README, simplify modal close

--- README.md
+++ README.md
@@ -5,9 +5,7 @@ The third iteration of the Advanced Cockpit Emulator.
 1. [Install Rust and toolchain](https://tauri.app/v1/guides/getting-started/prerequisites)
 2. Clone the repo and `cd` into it
 3. `npm i`
-4. `corepack enable` - Tauri wants `yarn`, `corepack` helps to manage all the different package managers. 
-    Comes with Node 16.10+. You can install `yarn` manually if don't want to use `corepack` too.
-5. `npm start`
+4. `npm start`
 
 ## Recommended IDE Setup for development
 
--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,10 +1,9 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { FiX } from 'react-icons/fi';
 import { AnimatePresence, motion, Variants } from 'framer-motion';
 
 const KEY_NAME_ESC = 'Escape';
 const KEY_EVENT_TYPE = 'keyup';
-const MOUSE_UP = 'mouseup';
 
 const modalBgAnimation: Variants = {
     hidden: {
@@ -36,7 +35,6 @@ export interface ModalProps {
 }
 
 export const Modal: React.FC<ModalProps> = ({ title, show, onExit, children }) => {
-    const ref = useRef<HTMLDivElement>(null);
     // Close modal on Esc press
     const handleEscKey = useCallback((event: KeyboardEvent) => {
         if (event.key === KEY_NAME_ESC) {
@@ -44,23 +42,13 @@ export const Modal: React.FC<ModalProps> = ({ title, show, onExit, children }) =
         }
     }, [onExit]);
 
-    // Close on outside click
-    const handleClick = useCallback((event: MouseEvent) => {
-        // @ts-ignore EventTarget is not mapping on Node but works just fine
-        if (ref?.current?.contains && !ref.current.contains(event.target)) {
-            onExit();
-        }
-    }, [onExit, ref]);
-
     useEffect(() => {
         document.addEventListener(KEY_EVENT_TYPE, handleEscKey, false);
-        document.addEventListener(MOUSE_UP, handleClick);
 
         return () => {
             document.removeEventListener(KEY_EVENT_TYPE, handleEscKey, false);
-            document.removeEventListener(MOUSE_UP, handleClick);
         };
-    }, [handleClick, handleEscKey]);
+    }, [handleEscKey]);
 
     return (
         <AnimatePresence>
@@ -72,6 +60,7 @@ export const Modal: React.FC<ModalProps> = ({ title, show, onExit, children }) =
                     initial="hidden"
                     animate="visible"
                     exit="hidden"
+                    onClick={onExit}
                 >
                     <motion.div
                         className="relative max-w-1/2 mx-auto bg-silver-800 rounded-2xl shadow-2xl"
@@ -85,11 +74,14 @@ export const Modal: React.FC<ModalProps> = ({ title, show, onExit, children }) =
                         initial="hidden"
                         animate="visible"
                         exit="hidden"
-                        ref={ref}
                     >
                         <div className="px-8 py-5 bg-silver-700 rounded-t-2xl flex justify-between items-center">
                             <h3 className="font-medium">{title}</h3>
-                            <FiX size={30} className="text-silver-400 cursor-pointer hover:text-silver-200" onClick={onExit} />
+                            <FiX
+                                size={30}
+                                className="text-silver-400 cursor-pointer hover:text-silver-200"
+                                onClick={onExit}
+                            />
                         </div>
                         <div className="p-8">
                             {children}
